a64998d7ff4569ae1756615fee026c35658264cc
brightowsus <owusub230@gmail.com>
2025-05-18 10:47:40 +0000

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,15 +6,22 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+public static double[] MultiplesOf(double number, int length)
+{
+    // Step 1: Create an array of doubles with size 'length'
+    double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+    // Step 2: Loop from 0 to length-1
+    for (int i = 0; i < length; i++)
+    {
+        // Step 3: Each element is number multiplied by (i + 1)
+        multiples[i] = number * (i + 1);
     }
+
+    // Step 4: Return the resulting array
+    return multiples;
+}
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -23,11 +30,15 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
-    {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-    }
+public static void RotateListRight(List<int> data, int amount)
+{
+    // Step 1: Get the last 'amount' elements using GetRange
+    List<int> lastElements = data.GetRange(data.Count - amount, amount);
+
+    // Step 2: Remove the last 'amount' elements
+    data.RemoveRange(data.Count - amount, amount);
+
+    // Step 3: Insert the removed elements at the beginning of the list
+    data.InsertRange(0, lastElements);
+}
 }

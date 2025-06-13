d7c40b09e4dc0f205b84e6fb9ff94534b47d4955
brightowsus <owusub230@gmail.com>
2025-06-13 06:34:42 +0000

    Translator

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -24,7 +24,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord.ToLower()] = toWord;
     }
 
     /// <summary>
@@ -32,9 +32,13 @@ public class Translator
     /// </summary>
     /// <param name="fromWord">The word to translate</param>
     /// <returns>The translated word or "???" if no translation is available</returns>
-    public string Translate(string fromWord)
+public string Translate(string fromWord)
+{
+    string key = fromWord.ToLower();
+    if (_words.ContainsKey(key))
     {
-        // ADD YOUR CODE HERE
-        return "";
+        return _words[key];
     }
+    return "???";
+}
 }
